dc3a64aff3934780f367f27cd38c0992bf89aa1f
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2022-09-12 18:51:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function mani(){
+    zed=document.getElementById("sol").value;
+    addison=document.getElementById("luna").value;
+    localStorage.setItem("zed",zed);
+    localStorage.setItem("addison",addison);
+    window.location="prueba.html";
+ }
